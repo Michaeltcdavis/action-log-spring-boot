7d0728e08d8edaef40b17afd6fdec203f236c240
Michael Davis <michaeltcdavis@gmail.com>
2023-10-10 19:34:07 -0400

    test and debug routes

---
+++ src/main/java/com/davcott/actionLog/user/UserController.java
@@ -0,0 +1,32 @@
+package com.davcott.actionLog.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(path = "/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository users;
+
+    @PostMapping(path = "/")
+    public @ResponseBody String addUser(
+            @RequestParam String name,
+            @RequestParam String email
+        ) {
+        User n = new User();
+        n.setUserName(name);
+        n.setEmail(email);
+        users.save(n);
+        return "Saved";
+    }
+
+    @GetMapping(path = "/")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return users.findAll();
+    }
+
+
+}
