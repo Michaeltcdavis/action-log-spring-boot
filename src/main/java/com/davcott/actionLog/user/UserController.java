dd064211b142f83d899d62b531df321836ffbdcd
Michael Davis <michaeltcdavis@gmail.com>
2023-10-10 19:38:55 -0400

    add create date to new user path

--- src/main/java/com/davcott/actionLog/user/UserController.java
+++ src/main/java/com/davcott/actionLog/user/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
+
 @Controller
 @RequestMapping(path = "/users")
 public class UserController {
@@ -19,6 +21,7 @@ public class UserController {
         User n = new User();
         n.setUserName(name);
         n.setEmail(email);
+        n.setCreatedDate(new Date());
         users.save(n);
         return "Saved";
     }
